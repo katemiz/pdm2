afbc5a19a9b515a18c06c6f30a6e5a88fad23a6d
Kılıç Ali Temiz <katemiz@gmail.com>
2024-12-17 22:14:35 +0300

    Added color and opacity to Hero Cards

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -47,7 +47,7 @@
 
 
 
-                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div class="max-w-sm bg-slate-300 opacity-50 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <a href="#">
                             <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
                         </a>
